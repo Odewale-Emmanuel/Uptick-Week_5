08cde8cb0f739c2e711968787c38464d44720a86
odewale emmanuel <odebzemmygee@gmail.com>
2025-08-04 17:10:14 +0100

    sign in

--- src/components/signin-form.tsx
+++ src/components/signin-form.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { data, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { LoaderIcon } from "lucide-react";
 import { useState } from "react";
 import { passwordRegEx, emailRegEx } from "@/utils/regex";
