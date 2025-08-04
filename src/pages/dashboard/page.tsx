178a0839b4ca30f87d1d5514bd77131f412131a4
odewale emmanuel <odebzemmygee@gmail.com>
2025-08-04 21:44:55 +0100

    user greetings and avatar

--- src/pages/dashboard/page.tsx
+++ src/pages/dashboard/page.tsx
@@ -5,17 +5,24 @@ import { jwtDecode } from "jwt-decode";
 //   LuSettings,
 //   LuSearch,
 // } from "react-icons/lu";
-// import avatar from "@/assets/avatar.jpg";
+import avatar from "@/assets/avatar.jpg";
+import { getGreeting } from "@/utils/get-greetings";
 import type { DecodedToken } from "@/types/decoded-token";
 
 const authToken = localStorage.getItem("authToken");
 const user = jwtDecode<DecodedToken>(authToken as string);
+
 function Dashboard() {
   return (
     <div>
       <aside className="">
-        <div>
-          <h1>welcome</h1>
+        <div className="">
+          <img
+            src={avatar}
+            alt="user-avatar"
+            className="aspect-square saturate-0 rounded-lg m-4 w-10"
+          />
+          <h1>{getGreeting(new Date())}</h1>
           <h1>{user.name}</h1>
           <p>{user.email}</p>
         </div>
