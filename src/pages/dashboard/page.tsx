9ab263112afc06e88fafa901d3f711400f2938f5
odewale emmanuel <odebzemmygee@gmail.com>
2025-08-04 18:38:49 +0100

    dashboard setup

--- src/pages/dashboard/page.tsx
+++ src/pages/dashboard/page.tsx
@@ -1,7 +1,29 @@
+import { jwtDecode } from "jwt-decode";
+import {
+  LuNotebook,
+  LuNotebookPen,
+  LuSettings,
+  LuSearch,
+} from "react-icons/lu";
+import avatar from "@/assets/avatar.jpg";
+import type { DecodedToken } from "@/types/decoded-token";
+
+const authToken = localStorage.getItem("authToken");
+const user = jwtDecode<DecodedToken>(authToken as string);
 function Dashboard() {
   return (
     <div>
-      <p className="">welcome to your personal Dashboard</p>
+      <aside className="">
+        <div>
+          <h1>welcome</h1>
+          <h1>{user.name}</h1>
+          <p>{user.email}</p>
+        </div>
+      </aside>
+      <section>
+        <div></div>
+        <div></div>
+      </section>
     </div>
   );
 }
---
+++ src/types/decoded-token.ts
@@ -0,0 +1,8 @@
+interface DecodedToken {
+  _id: string;
+  name: string;
+  email: string;
+  iat: number;
+}
+
+export type { DecodedToken };
